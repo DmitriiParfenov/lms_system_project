a8e3a629888c11adee3ea6868b8f923105525dc3
Dmitrii <dima12sik@yandex.ru>
2023-08-30 00:52:17 +0300

    updated admin.py

--- course/admin.py
+++ course/admin.py
@@ -6,7 +6,7 @@ from course.models import Course
 # Register your models here.
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'description')
+    list_display = ('title', 'description')
     list_display_links = ('title', )
     search_fields = ('title', )
-    list_filter = ('pk', 'title')
+    list_filter = ('title', )
--- lesson/admin.py
+++ lesson/admin.py
@@ -6,7 +6,7 @@ from lesson.models import Lesson
 # Register your models here.
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'description', 'url_video')
+    list_display = ('title', 'description', 'url_video')
     list_display_links = ('title', 'url_video')
     search_fields = ('title', 'url_video')
-    list_filter = ('pk', 'title')
+    list_filter = ('title', )
