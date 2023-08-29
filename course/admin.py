466faf55e4d829e0bc4f6f4605c7510f1f43853a
Dmitrii <dima12sik@yandex.ru>
2023-08-30 00:46:28 +0300

    registered Course in the admin panel

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from course.models import Course
+
+
 # Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description')
+    list_display_links = ('title', )
+    search_fields = ('title', )
+    list_filter = ('pk', 'title')
