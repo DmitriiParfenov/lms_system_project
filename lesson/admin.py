f3ec2f21539918730b7638ddb9759d119186114f
Dmitrii <dima12sik@yandex.ru>
2023-08-30 00:50:27 +0300

    registered Lesson in the admin panel

--- lesson/admin.py
+++ lesson/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from lesson.models import Lesson
+
+
 # Register your models here.
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description', 'url_video')
+    list_display_links = ('title', 'url_video')
+    search_fields = ('title', 'url_video')
+    list_filter = ('pk', 'title')
