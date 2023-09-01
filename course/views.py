2521b76238466856a37da38b40f89f1d3b665b88
Dmitrii <dima12sik@yandex.ru>
2023-09-01 22:37:45 +0300

    add view for the CourseSerializerByLessonId

--- course/views.py
+++ course/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from course.models import Course
-from course.serializers import CourseSerializer
+from course.serializers import CourseSerializer, CourseSerializerByLessonId
 
 
 # Create your views here.
 class CourseViewSet(viewsets.ModelViewSet):
+    """Для создание / редактирования / обновления / удаления объектов модели Course."""
+
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+
+class CourseCreateAPIViewByLessonId(generics.CreateAPIView):
+    """Generic для создания объекта модели Course с добавлением связанного поля lesson по id."""
+
+    serializer_class = CourseSerializerByLessonId
+
