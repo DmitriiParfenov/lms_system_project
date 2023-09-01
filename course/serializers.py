7469752f813c0a2380245ae13880f6198ccc7cee
Dmitrii <dima12sik@yandex.ru>
2023-09-01 22:37:11 +0300

    add docstrings updated create method for the CourseSerializer: add count_lesson field and detailed info about lesson of the Course model add CourseSerializerByLessonId for create object of the Course with lesson field by lesson_id

--- course/serializers.py
+++ course/serializers.py
@@ -1,9 +1,43 @@
 from rest_framework import serializers
 
 from course.models import Course
+from lesson.models import Lesson
+from lesson.serializers import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    """Класс сериализут поля модели Course: <title>, <description>. Поле <lesson> сериализуется через вложенный
+    сериализатор LessonSerializer с полным описанием полей вложенного сериализатора, при чем, если при создании
+    объекта модели Course и при описании всех полей вложенного сериализатора для Lesson не существует объекта
+    модели Lesson, то он будет создан. Поле <count_lesson> — это общее количество уроков в курсе."""
+
+    lesson = LessonSerializer(many=True)
+    count_lesson = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
-        fields = ('title', 'description', 'lesson')
+        fields = ('title', 'description', 'count_lesson', 'lesson',)
+
+    def create(self, validated_data):
+        """Метод создает объект модели Course с указанием полей для вложенного объекта модели Lesson. Если текущий
+        объект есть в базе данных, будет возбуждено исключение о существование такого объекта, если нет, то будет
+        создан объект модели Lesson."""
+
+        lesson_data = validated_data.pop('lesson')
+        course = Course.objects.create(**validated_data)
+
+        for data in lesson_data:
+            lesson_object = Lesson.objects.create(**data)
+            course.lesson.add(lesson_object)
+
+        return course
+
+    def get_count_lesson(self, instance):
+        """Метод возвращает общее количество связанных полей lesson для текущего объекта Course."""
+        return instance.lesson.count()
+
+
+class CourseSerializerByLessonId(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('title', 'description', 'lesson',)
