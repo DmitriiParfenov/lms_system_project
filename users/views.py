9b9f62170dc5108e1757d895f61f144d0ccee720
Dmitrii <dima12sik@yandex.ru>
2023-09-01 22:39:22 +0300

    add CRUD generics views for the UserSerializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
+from rest_framework import generics
 from django.shortcuts import render
 
+from users.models import User
+from users.serializers import UserSerializer
+
+
 # Create your views here.
+class UserCreateAPIview(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserListAPIview(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIview(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIview(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDeleteAPIview(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
