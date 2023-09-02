5c6f88697eed4adb23d10b5a44e558f8c99d57ac
Dmitrii <dima12sik@yandex.ru>
2023-09-02 18:36:50 +0300

    deleted UserCreateAPIView and path to them

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserCreateAPIview, UserListAPIview, UserRetrieveAPIview, UserDeleteAPIview, UserUpdateAPIview
+from users.views import UserListAPIview, UserRetrieveAPIview, UserDeleteAPIview, UserUpdateAPIview
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', UserListAPIview.as_view(), name='user_list'),
     path('<int:pk>/', UserRetrieveAPIview.as_view(), name='user_detail'),
-    path('create_lesson/', UserCreateAPIview.as_view(), name='user_create'),
     path('update/<int:pk>/', UserUpdateAPIview.as_view(), name='user_delete'),
     path('delete/<int:pk>/', UserDeleteAPIview.as_view(), name='user_delete')
 ]
--- users/views.py
+++ users/views.py
@@ -1,31 +1,29 @@
 from rest_framework import generics
-from django.shortcuts import render
 
 from users.models import User
 from users.serializers import UserSerializer
 
 
 # Create your views here.
-class UserCreateAPIview(generics.CreateAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-
 class UserListAPIview(generics.ListAPIView):
+    """Для получения всех объектов модели User."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserRetrieveAPIview(generics.RetrieveAPIView):
+    """Для детализацаии 1 объекта модели User."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserUpdateAPIview(generics.UpdateAPIView):
+    """Для обновления объектов модели User."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserDeleteAPIview(generics.DestroyAPIView):
+    """Для удаления объектов модели User."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
