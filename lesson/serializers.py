b6f6d1011b86a4bce2d4b6eb87cc0d43ca8c6b9f
Dmitrii <dima12sik@yandex.ru>
2023-09-08 18:09:33 +0300

    add LessonListSerializer

--- lesson/serializers.py
+++ lesson/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from lesson.models import Lesson
+from users.models import User
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = ('id', 'title', 'description', 'url_video')
+
+
+class LessonListSerializer(serializers.ModelSerializer):
+    user_lesson = serializers.SlugRelatedField(slug_field='email', queryset=User.objects.all())
+
+    class Meta:
+        model = Lesson
+        fields = ('id', 'title', 'description', 'url_video', 'user_lesson')
