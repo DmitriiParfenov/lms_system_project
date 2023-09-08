e12e508305e15b6b60ffaadf38aa9f3d7adbd6b3
Dmitrii <dima12sik@yandex.ru>
2023-09-08 18:17:10 +0300

    add docstrings

--- payments/permissions.py
+++ payments/permissions.py
@@ -2,6 +2,9 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
+    """
+    Доступ разрешен только аутентифицированным пользователям. Детали записей могут смотреть только владельцы записи.
+    """
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return True
