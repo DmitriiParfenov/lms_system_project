c9e9a830cb0075e4abf974c17b1ede30e96ea506
Dmitrii <dima12sik@yandex.ru>
2023-09-08 18:07:39 +0300

    add user_lesson field

--- lesson/models.py
+++ lesson/models.py
@@ -5,10 +5,11 @@ from users.models import NULLABLE
 
 # Create your models here.
 class Lesson(models.Model):
-    title = models.CharField(max_length=150, verbose_name='название', unique=True)
+    title = models.CharField(max_length=150, verbose_name='название', )
     description = models.TextField(max_length=1000, verbose_name='описание')
     picture = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
     url_video = models.CharField(max_length=250, verbose_name='ссылка')
+    user_lesson = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
 
     def __str__(self):
         return self.title
