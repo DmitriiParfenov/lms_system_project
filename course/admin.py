a460a79ad10f36f48250def8832f2a5cacb94002
Dmitrii <dima12sik@yandex.ru>
2023-09-08 17:04:46 +0300

    add id field to the list_display in th admin panel

--- course/admin.py
+++ course/admin.py
@@ -6,7 +6,7 @@ from course.models import Course
 # Register your models here.
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')
+    list_display = ('id', 'title', 'description', 'user_course')
     list_display_links = ('title', )
     search_fields = ('title', )
     list_filter = ('title', )
