498c3d7b5b3a43f3227bad8d3fcaa622d05ab588
Dmitrii <dima12sik@yandex.ru>
2023-09-08 18:14:35 +0300

    add id and user fields to the list_display

--- payments/admin.py
+++ payments/admin.py
@@ -6,6 +6,6 @@ from payments.models import Payment
 # Register your models here.
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('amount', 'is_lesson_paid', 'is_course_paid', 'payment_method')
+    list_display = ('id', 'amount', 'is_lesson_paid', 'is_course_paid', 'payment_method', 'user')
     list_display_links = ('amount', 'is_lesson_paid', 'is_course_paid', 'payment_method')
     search_fields = ('amount', 'is_lesson_paid', 'is_course_paid', 'payment_method')
