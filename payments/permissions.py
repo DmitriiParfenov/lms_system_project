b67dee428c29c7a8408a6b1b08c63ccb4022818d
Dmitrii <dima12sik@yandex.ru>
2023-09-08 18:15:08 +0300

    add IsOwner custom permissions class

---
+++ payments/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.user:
+            return True
+        return False
