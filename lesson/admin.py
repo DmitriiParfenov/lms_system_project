6babc46f760e3668a8035e2a7c007bf13a9980ae
Dmitrii <dima12sik@yandex.ru>
2023-09-08 18:06:19 +0300

    add id fields to the list_display

--- lesson/admin.py
+++ lesson/admin.py
@@ -6,7 +6,7 @@ from lesson.models import Lesson
 # Register your models here.
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'url_video')
+    list_display = ('id', 'title', 'description', 'url_video', 'user_lesson')
     list_display_links = ('title', 'url_video')
     search_fields = ('title', 'url_video')
     list_filter = ('title', )
