dd69558ffca7a4e85f4e23a29f2662b9f10e8fb8
Dmitrii <dima12sik@yandex.ru>
2023-09-15 13:20:12 +0300

    added get_session func to create the 'session' object for payment using API. added retrieve_session func to get the 'session' object from id using API.

--- payments/services.py
+++ payments/services.py
@@ -1,4 +1,8 @@
+import os
+
 from django_filters import rest_framework
+import stripe
+from rest_framework import serializers
 
 from payments.models import Payment
 
@@ -7,14 +11,58 @@ PAYMENT_METHOD_CHOICES = (('Наличные', 'Наличные'), ('Перев
 
 class PaymentFilter(rest_framework.FilterSet):
     """Класс для фильтрации полей модели Payment:
-    1) payment_method — выбор способа оплаты, наличные или перевод;
-    2) is_lesson_paid — исключает из общей выборки те записи, к оторых не оплачен урок;
-    3) is_course_paid — исключает из общей выборки те записи, к оторых не оплачен курс."""
+    1) payment_method — выбор способа оплаты, наличные или перевод.
+    2) lesson — исключает из общей выборки те записи, у которых не оплачен урок.
+    3) course — исключает из общей выборки те записи, у которых не оплачен курс."""
 
     payment_method = rest_framework.TypedChoiceFilter(choices=PAYMENT_METHOD_CHOICES)
-    is_lesson_paid = rest_framework.BooleanFilter(lookup_expr='isnull', exclude=True)
-    is_course_paid = rest_framework.BooleanFilter(lookup_expr='isnull', exclude=True)
+    lesson = rest_framework.BooleanFilter(lookup_expr='isnull', exclude=True)
+    course = rest_framework.BooleanFilter(lookup_expr='isnull', exclude=True)
 
     class Meta:
         model = Payment
-        fields = ('payment_method', 'is_lesson_paid', 'is_course_paid')
+        fields = ('payment_method', 'lesson', 'course')
+
+
+def get_session(instance):
+    """
+    Метод возвращает сессию для оплаты курса и / или урока по API.
+    """
+    stripe.api_key = os.getenv('STRIPE_API_KEY')
+
+    title_product = f'{instance.lesson}' if instance.lesson else ''
+    title_product += f'{instance.course}' if instance.course else ''
+
+    product = stripe.Product.create(name=f"{title_product}")
+    price = stripe.Price.create(
+        unit_amount=instance.amount,
+        currency="rub",
+        product=f"{product.id}",
+    )
+
+    session = stripe.checkout.Session.create(
+        success_url="https://example.com/success",
+        line_items=[
+            {
+                "price": f"{price.id}",
+                "quantity": 1,
+            },
+        ],
+        mode="payment",
+        customer_email=f"{instance.user.email}"
+    )
+
+    return session
+
+
+def retrieve_session(session):
+    """
+    Метод возвращает объект сессии по API, id которого был передан в качестве аргумента функции.
+    """
+    stripe.api_key = os.getenv('STRIPE_API_KEY')
+
+    return stripe.checkout.Session.retrieve(
+        session,
+    )
+
+
