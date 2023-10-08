c03b89d6eb72e77a6bef28d7084bb185abefa5df
Dmitrii <dima12sik@yandex.ru>
2023-10-08 21:26:13 +0300

    updated Periodic tasks schedule

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -15,3 +16,13 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+# celery app configuration
+app.conf.CELERYBEAT_SCHEDULE = {
+    'Inactivation users': {
+        'task': 'tasks.inactivate_user',
+        'schedule': crontab(hour='15', minute='30')
+    },
+}
+
+app.conf.CELERY_TIMEZONE = 'Australia/Tasmania'
